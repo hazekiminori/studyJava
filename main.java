9a21e9b66345b931b893e33b02cde16df36e7235
hazekiminori <minori.66@gmail.com>
2023-03-23 12:21:34 +0000

    lesson1

--- main.java
+++ main.java
@@ -1,5 +1,27 @@
 public class main{
 public static void main(String[] args){
   System.out.println("Hello World!");
+  
+  System.out.println(3);
+  System.out.println(5 + 2);
+  System.out.println(8 - 5);
+  System.out.println(3 * 2);
+  System.out.println(6 / 2);
+  System.out.println(8 % 5);
+  System.out.println("こんにちは" + "世界");
+  System.out.println("38" + "19");
+  System.out.println(38 + 19);
+  
+  int number1 = 3;
+  System.out.println(number1);
+  
+  String name = "Wanko";
+  System.out.println(name);
+  
+  int number2 = 7;
+  System.out.println(number1 * number2);
+  
+  String text = "プログラミングを勉強しよう";
+  System.out.println("Progateで" + text);
 }
 } 
